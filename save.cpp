dc3ee9faa083cbfe6b02ceff182abdd400f85151
Miguel Silva <msilva@msilva.internal.hq.grupopie.com>
2013-05-14 18:28:50 +0100

    Fourth commit

--- save.cpp
+++ save.cpp
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "save.h"
 #include "autostory.h"
 
 int load_character(FILE *file, state_t *state)
 {
+  char line[100];
+  character_t *character;
+  character = &state->characters[state->nr_characters++];
+  while (!feof(file)) {
+    fscanf(file, "%[^\n]\n", line);
+    if (strcmp(line, "__END__") == 0)
+      return 0;
+  }
+  fprintf(stderr, "Error: unexpected end of file when loading character\n");
+  return -1;
 }
 
 int load_saved_state(const char *filename, state_t *state)
@@ -18,9 +29,18 @@ int load_saved_state(const char *filename, state_t *state)
     fprintf(stderr, "Error openning file %s\n", filename);
     return -1;
   }
-  fscanf(file, "MAX_CHARACTERS=%d\n", state->max_characters);
-  fscanf(file, "MAX_NATIONS=%d\n", state->max_nations);
-  fscanf(file, "MAX_LOCATIONS=%d\n", state->max_locations);
+  if (fscanf(file, "MAX_CHARACTERS = %d\n", state->max_characters) != 1) {
+    fprintf(stderr, "Error: missing max characters setting\n");
+    return -1;
+  }
+  if (fscanf(file, "MAX_NATIONS=%d\n", state->max_nations) != 1) {
+    fprintf(stderr, "Error: missing max nations setting\n");
+    return -1;
+  }
+  if (fscanf(file, "MAX_LOCATIONS=%d\n", state->max_locations) != 1) {
+    fprintf(stderr, "Error: missing max locations setting\n");
+    return -1;
+  }
   while (!feof(file)) {
     fscanf(file, "%[^\n]\n", line);
     if (strcmp(line, "__CHARACTER__") == 0) {
@@ -36,6 +56,6 @@ int load_saved_state(const char *filename, state_t *state)
       fprintf(stderr, "Unknown structure %s\n", line);
     }
   }
-  fprintf(stderr, "\n");
+  fprintf(stderr, "Loading done\n");
   return 0;
 }
